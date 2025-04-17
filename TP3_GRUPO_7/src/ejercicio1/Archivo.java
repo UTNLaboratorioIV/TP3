383f61c8b85dee7e15607547b7c30a0af6a025ee
MaximilianoAns <127036656+MaximilianoAns@users.noreply.github.com>
2025-04-17 00:25:23 -0300

    Ejercicio 1 parcial. Se agrego la clase archivo las funciones para leer el archivo, letra por letra, salto de linea para visualizar mejor la lectura y funcion para visualizar los datos sin duplicados

--- TP3_GRUPO_7/src/ejercicio1/Archivo.java
+++ TP3_GRUPO_7/src/ejercicio1/Archivo.java
@@ -1,5 +1,79 @@
 package ejercicio1;
 
-public class Archivo {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Set;
 
+public class Archivo {
+	private String ruta;
+	
+	
+	public String getRuta() {
+		return ruta;
+	}
+
+
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
+	}
+
+
+	public boolean existe() {
+		File archivo = new File(ruta); 
+		if(archivo.exists()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void leer_archivoXLetra() {
+		FileReader entrada;
+		try {
+			entrada = new FileReader(ruta);
+			int l = entrada.read(); // lee el primer caracter 
+			while(l != -1) {
+				char letra = (char) l;
+				System.out.println(letra);
+				l = entrada.read();
+			}
+			entrada.read();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void leerArchivoPorLinea() {
+	    try {
+	        BufferedReader lector = new BufferedReader(new FileReader(ruta));
+	        String linea;
+	        while ((linea = lector.readLine()) != null) {
+	            System.out.println(linea); // muestra la línea completa
+	        }
+	        lector.read();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	public void leerSinDuplicados() {
+	    Set<String> nombresUnicos = new HashSet<>();
+	    
+	    try {
+	        BufferedReader leer = new BufferedReader(new FileReader(ruta));
+	        String linea;
+	        while ((linea = leer.readLine()) != null) {
+	            if (nombresUnicos.add(linea.trim())) { //con trim eliminamos espacios al principio y final 
+	                System.out.println(linea);
+	            }
+	        }
+	        leer.read();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	
 }
--- TP3_GRUPO_7/src/ejercicio1/Main.java
+++ TP3_GRUPO_7/src/ejercicio1/Main.java
@@ -4,7 +4,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-
+			//EJERCICIO 2 LEER ARCHIVO PERSONAS TXT
+		Persona p1 = new Persona();
+		Archivo a1 = new Archivo();
+		
+		a1.setRuta("C:\\Users\\Lenovo\\Desktop\\PORFOLIONEW\\TPWEB\\TP3\\TP3_GRUPO_7\\src\\Archivo\\personas.txt");
+		if(a1.existe()) {
+			System.out.println("El archivo ya existe!");
+		}else {
+			System.out.println("El archivo NO existe!");
+		}
+		
+		System.out.println(" Lectura del archivo: ");
+		a1.leerArchivoPorLinea();
+		
+		System.out.println(" CORTE PARA VERIFICAR DUPLICADOS  ");
+		a1.leerSinDuplicados();
+		
+		
 	}
+
 
 }
